543835aea9504a1593f687e293f2e8dcf5c8355d
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-02-04 21:05:56 +0100

    Primer comit frontal para node express

---
+++ README.md
@@ -0,0 +1,12 @@
+## 💻 ToDo List multi API
+Mediante distintas APIs, hechas con Express, KOA, HAPI, Meteor, Restify y mas estaremos utilizando esta lista.
+
+### Mis cursos de APIs
+
+⭐ https://www.udemy.com/user/nahuellema/
+
+---
+
+### Contacto
+
+👋 [Nahuel Lema](https://www.linkedin.com/in/nahuellema/)
---
+++ modules/tasks/tasks.service.js
@@ -0,0 +1,92 @@
+$('document').ready(() => {
+  window.API_PATH = (() => {
+    var host = location.host;
+    if (host.includes('localhost')) {
+      host = 'http://localhost:5001/';
+    } else if (host.includes('herokuapp')) {
+      const href = location.href;
+      if (href.includes('koa')) {
+        host = 'nl-koa-api-prod';
+      } else if (href.includes('hapi')) {
+        host = 'nl-hapi-api-prod';
+      } else if (href.includes('restana')) {
+        host = 'nl-restana-api';
+      } else if (href.includes('fastify')) {
+        host = 'nl-fastify-api';
+      } else if (href.includes('restify')) {
+        host = 'nl-restify-api';
+      } else if (href.includes('feathers')) {
+        host = 'nl-feathers-api';
+      } else if (href.includes('adonis')) {
+        host = 'nl-adonis-api';
+      } else {
+        host = 'nl-express-api';
+      }
+      host = `https://${host}.herokuapp.com/`;
+    }
+    return host;
+  })();
+  window.API_TASKS = window.API_PATH + 'tasks';
+
+  window.TASKS_SRV = {
+    create: function (params) {
+      return $.ajax({
+        url: window.API_TASKS,
+        method: 'post',
+        data: params.task
+      });
+    },
+    list: function (params) {
+      return $.ajax({
+        url: window.API_TASKS,
+        method: 'get',
+        data: params
+      });
+    },
+    get: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId,
+        method: 'get',
+        data: params
+      });
+    },
+    update: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId,
+        method: 'put',
+        data: params
+      });
+    },
+    delete: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId,
+        method: 'delete'
+      });
+    },
+    updateCompleted: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId + '/completed',
+        method: 'put',
+        data: params
+      });
+    },
+    updateTitle: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId + '/title',
+        method: 'put',
+        data: params
+      });
+    },
+    updateImages: function (params) {
+      return $.ajax({
+        url: window.API_TASKS + '/' + params.taskId + '/images',
+        type: 'put',
+        data: params.files,
+        dataType: 'html',
+        cache: false,
+        contentType: false,
+        processData: false
+      });
+    }
+  };
+});
